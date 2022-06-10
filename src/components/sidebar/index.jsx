6af3bf410821e2f5eeb7d95d2d9b6c437c7c9302
Miraxtamov <jmiraxtamov@gmail.com>
2022-06-10 23:11:50 +0500

    Add Sidebar

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import hamburgerBtn from "../../assets/icon/hamburgerBtn.svg";
 import LogoHome from "../../assets/icon/logo.svg";
-import { Container, Logo, LogoBar, LogoLink, SidebarContainer, SidebarHome } from "./style";
+import SidebarHomeIcon from "../../assets/icon/home.svg";
+import SidebarExploreIcon from "../../assets/icon/explore.svg";
+import SidebarSubscriptionsIcon from "../../assets/icon/subscriptions.svg";
+import SidebarLibraryIcon from "../../assets/icon/library.svg";
+import SidebarHistoryIcon from "../../assets/icon/history.svg";
+import SidebarYourVideosIcon from "../../assets/icon/your-videos.svg";
+import SidebarWatchLaterIcon from "../../assets/icon/watch-later.svg";
+import SidebarLikedIcon from "../../assets/icon/liked.svg";
+import SidebarLive from "../../assets/icon/live.svg";
+import SidebarVideoUploaded from "../../assets/icon/video-uploaded.svg";
+import SidebarArrowIcon from "../../assets/icon/arrow.svg";
+import Avatar1 from "../../assets/img/AvatarPhoto1.png";
+import Avatar2 from "../../assets/img/AvatarPhoto2.png";
+import Avatar3 from "../../assets/img/AvatarPhoto3.png";
+import Avatar4 from "../../assets/img/AvatarPhoto4.png";
+import Avatar5 from "../../assets/img/AvatarPhoto5.png";
+import {
+	Container,
+	Logo,
+	LogoBar,
+	LogoLink,
+	SidebarContainer,
+	SidebarHome,
+	SidebarIcon,
+	SidebarLi,
+	SidebarLine,
+	SidebarLink,
+	SidebarLiText,
+	SidebarTitle,
+	SidebarUl,
+	
+} from "./style";
 
 function Sidebar() {
 	return (
@@ -12,9 +43,134 @@ function Sidebar() {
 					<Logo title="YouTube Home" src={LogoHome} />
 				</LogoLink>
 			</LogoBar>
-      <SidebarContainer>
-        
-      </SidebarContainer>
+			<SidebarContainer>
+				<SidebarUl>
+					<SidebarLi firstLi>
+						<SidebarLink title="Home" href="#">
+							<SidebarIcon src={SidebarHomeIcon} alt="Home" />
+							<SidebarLiText>Home</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Explore" href="#">
+							<SidebarIcon src={SidebarExploreIcon} alt="Explore" />
+							<SidebarLiText>Explore</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Subscriptions" href="#">
+							<SidebarIcon src={SidebarSubscriptionsIcon} alt="Subscriptions" />
+							<SidebarLiText>Subscriptions</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+				</SidebarUl>
+				<SidebarLine />
+				<SidebarUl>
+					<SidebarLi>
+						<SidebarLink title="Library" href="#">
+							<SidebarIcon src={SidebarLibraryIcon} alt="Library" />
+							<SidebarLiText>Library</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="History" href="#">
+							<SidebarIcon src={SidebarHistoryIcon} alt="History" />
+							<SidebarLiText>History</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Your videos" href="#">
+							<SidebarIcon src={SidebarYourVideosIcon} alt="Your videos" />
+							<SidebarLiText>Your videos</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Watch Later" href="#">
+							<SidebarIcon src={SidebarWatchLaterIcon} alt="Watch Later" />
+							<SidebarLiText>Watch Later</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Liked Videos" href="#">
+							<SidebarIcon src={SidebarLikedIcon} alt="Liked Videos" />
+							<SidebarLiText>Liked Videos</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Show more" href="#">
+							<SidebarIcon src={SidebarArrowIcon} alt="Show more" />
+							<SidebarLiText>Show more</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+				</SidebarUl>
+				<SidebarLine mbT />
+				<SidebarTitle>SUBSCRIPTIONS</SidebarTitle>
+				<SidebarUl>
+					<SidebarLi subsMT>
+						<SidebarLink title="The Game" href="#">
+							<SidebarIcon src={Avatar1} alt="The Game" />
+							<SidebarLiText>The Game</SidebarLiText>
+							<SidebarIcon alignRight src={SidebarLive} alt="The Game" />
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Dev Ed" href="#">
+							<SidebarIcon src={Avatar2} alt="Dev Ed" />
+							<SidebarLiText>Dev Ed</SidebarLiText>
+							<SidebarIcon
+								alignRight
+								src={SidebarVideoUploaded}
+								alt="The Game"
+							/>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Dani Krossing" href="#">
+							<SidebarIcon src={Avatar3} alt="Dani Krossing" />
+							<SidebarLiText>Dani Krossing</SidebarLiText>
+							<SidebarIcon
+								alignRight
+								src={SidebarVideoUploaded}
+								alt="The Game"
+							/>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="CodingNepal" href="#">
+							<SidebarIcon src={Avatar4} alt="CodingNepal" />
+							<SidebarLiText>CodingNepal</SidebarLiText>
+							<SidebarIcon
+								alignRight
+								src={SidebarVideoUploaded}
+								alt="The Game"
+							/>
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Bedimcode" href="#">
+							<SidebarIcon src={Avatar5} alt="Bedimcode" />
+							<SidebarLiText>Bedimcode</SidebarLiText>
+							<SidebarIcon alignRight src={SidebarLive} alt="The Game" />
+						</SidebarLink>
+					</SidebarLi>
+					<SidebarLi>
+						<SidebarLink title="Show more" href="#">
+							<SidebarIcon src={SidebarArrowIcon} alt="Show more" />
+							<SidebarLiText>Show more</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+				</SidebarUl>
+				<SidebarLine mbT />
+				<SidebarTitle>MORE FROM YOUTUBE</SidebarTitle>
+				<SidebarUl>
+					<SidebarLi>
+						<SidebarLink title="YouTube Premium" href="#">
+							<SidebarIcon src={SidebarExploreIcon} alt="YouTube Premium" />
+							<SidebarLiText>YouTube Premium</SidebarLiText>
+						</SidebarLink>
+					</SidebarLi>
+				</SidebarUl>
+			</SidebarContainer>
 		</Container>
 	);
 }
--- src/components/sidebar/style.js
+++ src/components/sidebar/style.js
@@ -3,21 +3,43 @@ import styled from "styled-components";
 export const Container = styled.div`
 	position: sticky;
 	top: 0;
+	left: 0;
 	width: 240px;
 	height: 100vh;
 	background-color: #212121;
+	overflow: auto;
+
+	::-webkit-scrollbar {
+		width: 16px;
+	}
+
+	::-webkit-scrollbar-track {
+		border-radius: 8px;
+	}
+
+	::-webkit-scrollbar-thumb {
+		height: 56px;
+		border-radius: 8px;
+		border: 4px solid transparent;
+		background-clip: content-box;
+		background-color: #888;
+	}
 `;
 
 export const LogoBar = styled.div`
+	position: sticky;
+	top: 0;
+	background-color: #212121;
+	z-index: 1;
 	display: flex;
-	align-items: center
-  margin-bottom: 30px;
-  padding-top: 16px;
-  margin-left: 24px;
+	align-items: center;
+	padding-top: 16px;
+	padding-bottom: 16px;
 `;
 
 export const SidebarHome = styled.img`
 	cursor: pointer;
+	margin-left: 24px;
 `;
 
 export const LogoLink = styled.a``;
@@ -27,5 +49,63 @@ export const Logo = styled.img`
 `;
 
 export const SidebarContainer = styled.div`
-padding: 22px 40px 22px 26px;
+	z-index: 0;
+`;
+
+export const SidebarUl = styled.ul`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const SidebarLi = styled.li`
+	margin-top: ${(props) => (props.firstLi ? "9px" : "0")};
+	margin-top: ${(props) => (props.subsMT ? "8px" : {})};
+`;
+
+export const SidebarLink = styled.a`
+	display: flex;
+	align-items: center;
+	padding: 10px 40px 10px 27px;
+	font-size: 14px;
+	line-height: 20px;
+
+	&:hover {
+		background-color: #ffffff22;
+	}
+
+	&:focus {
+		font-weight: 500;
+		background-color: #ffffff33;
+	}
+`;
+
+export const SidebarIcon = styled.img`
+	width: ${(props) => (props.alignRight ? "10px" : "16px")};
+	height: ${(props) => (props.alignRight ? "10px" : "16px")};
+	margin-left: ${(props) => (props.alignRight ? "auto" : {})};
+`;
+
+export const SidebarLiText = styled.div`
+	color: #ffffff;
+	margin-left: 26px;
+	margin-top: 2px;
+`;
+
+export const SidebarLine = styled.hr`
+	border: 1px solid rgba(255, 255, 255, 0.2);
+	margin-top: 12px;
+	margin-bottom: ${(props) => (props.mbT ? "8px" : "22px")};
+`;
+
+export const SidebarTitle = styled.div`
+	color: rgba(255, 255, 255, 0.6);
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 16px;
+	padding-left: 27px;
+`;
+
+export const SidebarLive = styled.img`
+	/* width: auto;
+	height: auto; */
 `;
